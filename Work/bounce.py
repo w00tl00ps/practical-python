145117467ef2e8d7aa1eecad7ac64eea5b2fa89e
HY <w00tl00ps@gmail.com>
2020-08-02 16:13:12 -0400

    Finished lession 1.2

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+print('hello world')
+
+height = 100
+for x in range(10):
+  height = height *3/5
+  print(x+1, round(height,4))
---
+++ Work/sears.py
@@ -0,0 +1,15 @@
+# sears.py
+
+bill_thickness = 0.11 * 0.001    # Meters (0.11 mm)
+sears_height   = 442             # Height (meters)
+num_bills      = 1
+days            = 1
+
+while num_bills * bill_thickness < sears_height:
+    print(days, num_bills, num_bills * bill_thickness)
+    days = days + 1
+    num_bills = num_bills * 2
+
+print('Number of days', days)
+print('Number of bills', num_bills)
+print('Final height', num_bills * bill_thickness)
