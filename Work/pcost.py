d69c80b818478f910b76ac4f39cbe004c6012233
HY <w00tl00ps@gmail.com>
2020-08-03 22:52:22 -0400

    Working on part 1.7

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
-total_cost = 0
-s = ''
 
-f = open('Data/portfolio.csv','rt')
-header = f.readline()
-for line in f:
-  s = line.strip('\n').split(',')
-  print(s)
-  total_cost +=  float(s[1]) * float(s[2])
-  
-print(total_cost)
-  
-f.close()
+def portfolio_cost(filename):
+
+  total_cost = 0
+  s = ''
+
+  f = open(filename,'rt')
+  header = f.readline()
+  for line in f:
+    s = line.strip('\n').split(',')
+    print(s)
+    try:
+      total_cost +=  float(s[1]) * float(s[2])
+    except ValueError:
+      print("Couldn't parse", line)
+      
+    
+  print(total_cost)
+    
+  f.close()
+
