e52e8377c3c74778fca3f39cd9283e416a5510f4
HY <w00tl00ps@gmail.com>
2020-08-03 23:07:39 -0400

    Finished part 1.7 - Files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,7 +1,43 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
+
+
+def portfolio_cost(filename):
+
+  total_cost = 0
+  s = ''
+
+  f = open(filename)
+  rows = csv.reader(f)
+  
+  header = next(rows)
+  
+  for row in rows:
+    try:
+      total_cost += float(row[1]) * float(row[2])
+    except ValueError:
+      print("Couldn't parse", row)
+    
+  f.close()
+  return total_cost
+  
+  
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+print('System Arguments', sys.argv)
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)  
+  
+"""
 def portfolio_cost(filename):
 
   total_cost = 0
@@ -13,7 +49,8 @@ def portfolio_cost(filename):
     s = line.strip('\n').split(',')
     print(s)
     try:
-      total_cost +=  float(s[1]) * float(s[2])
+      shares = float(s[1])
+      total_cost +=  shares * float(s[2])
     except ValueError:
       print("Couldn't parse", line)
       
@@ -22,3 +59,4 @@ def portfolio_cost(filename):
     
   f.close()
 
+"""
