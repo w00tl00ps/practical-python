a004fc7d20a980a245433c281dd5ed7937b52505
HY <w00tl00ps@gmail.com>
2020-08-04 21:36:58 -0400

    Working on Exercise 2.5

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+  portfolio = []
+  
+  with open(filename, 'rt') as f:
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
+      portfolio.append( (row[0], int(row[1]), float(row[2]) ) )
+  
+  print(portfolio)
+  
+  return portfolio
+  
+
+read_portfolio('Data/portfolio.csv')
