833f3557c60858b43ba760cfeaa0467215558be8
HY <w00tl00ps@gmail.com>
2020-08-10 20:39:54 -0400

    Finished lesson 3.3 (Error Checking)

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -4,14 +4,19 @@
 
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True ):
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter2=',', silence_errors=False ):
   '''
   Parse a CSV file into a list of records
   '''
   records = []
   
+  #Raise error if no headers exist and header selection was specified
+  if select and has_headers==False:
+    raise RuntimeError('select argument requires column headers')
+    
+  
   with open(filename) as f:
-    rows = csv.reader(f)
+    rows = csv.reader(f, delimiter=delimiter2)
     
     # Read the file headers, if present
     if has_headers:
@@ -21,25 +26,34 @@ def parse_csv(filename, select=None, types=None, has_headers=True ):
       indices = []
       modified_headers = []
       for counter, col in enumerate(headers):
-        if col in select:
+        try:
+          if col in select:
+            indices.append(counter)
+            modified_headers.append(headers[counter]) #print(modified_headers)
+        except TypeError:
           indices.append(counter)
-          modified_headers.append(headers[counter]) #print(modified_headers)
+          modified_headers.append(headers[counter])
       
-      
+      rowCount = 1
       
       for row in rows:
           if not row:    # Skip rows with no data
               continue
           
-          #Handle type casting if specified
-          if types:
-            modified_row = [ types[i](row[i]) for i in indices ]
-          else:
-            modified_row = [ (row[i]) for i in indices ]
-          
+          try:
+            #Handle type casting if specified
+            if types:
+              modified_row = [ types[i](row[i]) for i in indices ]
+            else:
+              modified_row = [ (row[i]) for i in indices ]
+          except ValueError as e:
+            if not silence_errors:
+              print('Error processing row ', rowCount, row)
    
           record = dict(zip(modified_headers, modified_row))
           records.append(record)
+          rowCount += 1
+          
     elif not has_headers:
       for row in rows:
         record = tuple(row)
@@ -49,5 +63,13 @@ def parse_csv(filename, select=None, types=None, has_headers=True ):
 
 portfolio = parse_csv('Data/portfolio.csv', select = ['name', 'shares'])
 prices = parse_csv('Data/prices.csv', types=[str,float], has_headers=False)
+print('Portfolio 1')
 print(portfolio)
+print('Prices')
 print(prices)
+
+portfolio2 = parse_csv('Data/portfolio.dat', types=[str, int, float], delimiter2=' ')
+print('Portfolio2')
+print(portfolio2)
+
+prices2 = parse_csv('Data/missing.csv', types=[str, int, float], silence_errors=True)
